88147053627a83ef5cd1b53f292c33d316b64700
Peeyush Bhandari <72421807+peeyush987@users.noreply.github.com>
2024-11-22 09:31:00 +0530

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -13,26 +13,6 @@ def price(x):
     return '$%1.2f' % x
 
 
-def plot_basic(stocks, title='Google Trading', y_label='Price USD', x_label='Trading Days'):
-    """
-    Plots basic pyplot
-    :param stocks: DataFrame having all the necessary data
-    :param title:  Title of the plot 
-    :param y_label: yLabel of the plot
-    :param x_label: xLabel of the plot
-    :return: prints a Pyplot againts items and their closing value
-    """
-    fig, ax = plt.subplots()
-    ax.plot(stocks['Item'], stocks['Close'], '#0A7388')
-
-    ax.format_ydata = price
-    ax.set_title(title)
-
-    # Add labels
-    plt.ylabel(y_label)
-    plt.xlabel(x_label)
-
-    plt.show()
 
 
 def plot_prediction(actual, prediction, title='EW Trading vs Prediction', y_label='Price USD', x_label='Trading Days'):
